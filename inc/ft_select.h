18f477b7d2c821c3c892d7054385c79f9ef9a0cf
Geoffrey GANE <ggane@e1r5p6.42.fr>
2017-03-13 13:51:23 +0100

    ajout struct s_dlist servant de structure de control de la veritable liste doublement chainee struct s_cycle et des fonctions basique de creation de t_dlist, t_cycle, d'ajout de maillon et d'affichage de la liste.

--- inc/ft_select.h
+++ inc/ft_select.h
@@ -6,7 +6,7 @@
 /*   By: ggane <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 16:41:21 by ggane             #+#    #+#             */
-/*   Updated: 2017/01/09 17:11:56 by ggane            ###   ########.fr       */
+/*   Updated: 2017/03/13 13:24:40 by ggane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,24 +24,31 @@
 # include <sys/ioctl.h>
 # include "libft.h"
 
-typedef struct	s_cycle
+typedef struct		s_dlist
 {
-	char	*name;
-	int	line;
-	int	hover;
-	int	selected;
-	int	number;
-	s_cycle	*prev;
-	s_cycle	*next;
-}		t_cycle;
+	int				lenght;
+	struct s_cycle	*head;
+	struct s_cycle	*tail;
+}					t_dlist;
 
-void		open_ft_select_interface(t_cycle *args);
+typedef struct		s_cycle
+{
+	char			*name;
+	int				line;
+	int				hover;
+	int				selected;
+	int				node_number;
+	struct s_cycle	*prev;
+	struct s_cycle	*next;
+}					t_cycle;
+
+void				open_ft_select_interface(t_cycle *args);
 
 /*
 ** list_functions.c
 */
 
-t_cycle		*stock_arguments(int ac, char **av);
-void		erase_stocked_arguments(t_cycle **args);
+t_cycle				*stock_arguments(int ac, char **av);
+void				erase_stocked_arguments(t_cycle **args);
 
 #endif
--- src/list_functions.c
+++ src/list_functions.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list_functions.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggane <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/13 11:14:12 by ggane             #+#    #+#             */
+/*   Updated: 2017/03/13 13:49:02 by ggane            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_select.h"
 
-t_cycle		*stock_arguments(int ac, char **av)
+t_dlist		*stock_arguments(int ac, char **av)
 {
-	t_cycle	*args;
-	int	i;
+	t_dlist	*args;
+	int		i;
 
-	args = NULL;
 	i = 0;
 	if (ac == 1)
 		exit(1);
+	args = create_dlist();
 	while (i < ac - 1)
 	{
 		append_list(&args, create_new_element(av[i], i));
@@ -17,22 +29,80 @@ t_cycle		*stock_arguments(int ac, char **av)
 	return (args);
 }
 
-void		append_list(t_cycle **args, t_cycle *new)
+t_dlist		*create_dlist(void)
 {
-	t_cycle	*tmp;
+	t_dlist	*new;
 
-	if (!args)
+	if (!(new = (t_dlist *)malloc(sizeof(*new))))
+		return (NULL);
+	new->lenght = 0;
+	new->head = NULL;
+	new->tail = NULL;
+	return (new);
+}
+
+t_cycle		*create_new_element(char *arg, int nb)
+{
+	t_cycle		*new;
+
+	if (!(new = (t_cycle *)malloc(sizeof(*new))))
+		return (NULL);
+	new->name = ft_strdup(arg);
+	new->line = -1;
+	new->hover = -1;
+	new->selected = -1;
+	new->node_number = nb;
+	new->next = NULL;
+	new->prev = NULL;
+	return (new);
+}
+
+void		append_list(t_dlist **args, t_cycle *new)
+{
+	if ((*args)->tail)
 	{
-		args = new;
-		return ;
+		(*args)->tail->next = new;
+		new->prev = (*args)->tail;
+		(*args)->tail = new;
 	}
 	else
-		tmp = args;
-	while (tmp)
 	{
+		(*args)->head = new;
+		(*args)->tail = new;
+	}
+	(*args)->lenght++;
+}
+
+static void	display_info(t_cycle *element)
+{
+	ft_putendl("----------");
+	ft_putstr("name : ");
+	ft_putendl(element->name);
+	ft_putstr("line : ");
+	ft_putnbrdl(element->line);
+	ft_putstr("hover : ");
+	ft_putnbrdl(element->hover);
+	ft_putstr("selected : ");
+	ft_putnbrdl(element->selected);
+	ft_putstr("node_number : ");
+	ft_putnbrdl(element->node_number);
+	ft_putendl("----------");
+}
+
+void		print_list(t_dlist *args)
+{
+	t_cycle	*browse;
+
+	browse = args->head;
+	ft_putstr("list lenght : ");
+	ft_putnbrdl(args->lenght);
+	while (browse)
+	{
+		display_info(browse);
+		browse = browse->next;
 	}
 }
 
-void		erase_stocked_arguments(t_cycle **args)
+void		erase_stocked_arguments(t_dlist **args)
 {
 }
